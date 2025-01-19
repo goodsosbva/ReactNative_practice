e8087c35a7147211f1a46abe07a4d03155cbd366
khs <hs.kwan@okestro.com>
2025-01-20 01:11:35 +0900

    feat: 투두 표현, 투두 추가, 투가 퍼블리싱 완료

--- TodoApp/App.tsx
+++ TodoApp/App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {
     StatusBar,
     StyleSheet,
@@ -13,14 +13,39 @@ import {
     KeyboardAvoidingView,
     Platform
 } from 'react-native';
+import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
 import DateHead from './components/DateHead';
 import AddTodo from "./components/AddTodo";
 import Empty from "./components/Empty";
-import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
+import TodoList from "./components/TodoList";
 
 function App(): React.JSX.Element {
      const today  = new Date();
-     console.log(today);
+
+     const [todos, setTodos] = useState([
+         {id: 1, text: '작업환경 설정!', done: true},
+         {id: 2, text: '리액트 네이티브 기초 공부!', done: false},
+         {id: 3, text: '투두리시트 만들어보기', done: false},
+     ]);
+
+     const onInsert = (text: any) => {
+         const nextId = todos.length > 0 ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+
+         const todo = {
+             id: nextId,
+             text,
+             done: false,
+         };
+
+         setTodos(todos.concat(todo));
+     };
+
+     const onToggle = (id: any) => {
+         const nextTodos = todos.map(todo =>
+         todo.id === id ? {...todo, done: !todo.done} : todo,
+         );
+         setTodos(nextTodos);
+     }
 
   return (
     <SafeAreaProvider >
@@ -30,8 +55,8 @@ function App(): React.JSX.Element {
                 style={styles.avoid}
             >
                 <DateHead date={today} />
-                <Empty />
-                <AddTodo />
+                {todos.length === 0 ? <Empty /> : <TodoList todos={todos} onToggle={onToggle} />}
+                <AddTodo onInsert={onInsert} />
             </KeyboardAvoidingView>
         </SafeAreaView>
     </SafeAreaProvider>
@@ -45,7 +70,7 @@ const styles = StyleSheet.create({
     },
     avoid: {
         flex: 1,
-    }
+    },
 });
 
 export default App;
