0a0f52024178da18e534569770b6e15eabf5d5a1
khs <hs.kwan@okestro.com>
2025-04-04 23:27:04 +0900

    feat: 1. 프로파일에 커스텀 훅 적용, 2. 포스트 열기 기능 추가, 3. 내 프로필 화면 구현

--- publicGalleryGiantK/components/PostGriditem.js
+++ publicGalleryGiantK/components/PostGriditem.js
@@ -1,12 +1,14 @@
+import {useNavigation} from "@react-navigation/native";
 import React from 'react';
 import {StyleSheet, useWindowDimensions, Image, Pressable} from "react-native";
 
 function PostGriditem({post}) {
     const dimensions = useWindowDimensions();
     const size = (dimensions.width) / 3;
+    const navigation = useNavigation();
 
     const onPress = () => {
-        // TODO
+        navigation.navigate('Post', {post});
     }
 
     return (
