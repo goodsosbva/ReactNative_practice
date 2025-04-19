88f122d4db544c4742bd70edf00f10a5f364475e
khs <hs.kwan@okestro.com>
2025-04-19 18:24:22 +0900

    Feat: create the todos app using redux toolkit

--- StateManagement/App.tsx
+++ StateManagement/App.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import {Provider} from "react-redux";
 import {createStore} from 'redux';
 import AuthApp from './components/AuthApp.tsx'
-import rootReducer from "./slice";
+import TodoApp from "./components/TodoApp.tsx";
+import rootReducer from "./slices";
 
 const store = createStore(rootReducer);
 
 function App(): React.JSX.Element {
    return (
        <Provider store={store}>
-          <AuthApp />
+          <TodoApp />
        </Provider>
    )
 
